2fde1bdf18180de9492cecfc748d550115ec14ad
Tyrell <tyrell@localhost.localdomain>
2020-09-16 02:53:02 +0530

    added demo.py

---
+++ README.md
@@ -0,0 +1,30 @@
+# ASL Sign Language detector (CNN)
+
+## Made using PyTorch this simple model has achieved accuracy of 96% on test data.
+
+## Strting up
+
+Use python 3.7.6
+
+- Clone this repo
+- Get into parent directory of the repo
+- Create a virtual environment (preferably) and then use command
+```bash
+pip install -r requirements.txt -q
+```
+
+## For quick demo
+Put images to test in 'test_images' folder
+
+Then run
+```bash
+pip install opencv-python
+python demo.py 
+```
+
+## To retrain model
+```bash
+python train/main.py
+```
+
+## That's it folks
---
+++ demo.py
@@ -0,0 +1,36 @@
+import torch
+import torchvision.transforms import transforms
+import os
+import cv2
+from time import time
+
+test_transforms = transforms.Compose([transforms.Resize(28),
+                                      transforms.ToTensor()])
+
+model=torch.load('ckpt/sign_lang_lr_0.001_epo_25_0.pth')
+model.eval()
+
+def predict_image(image):
+    image_tensor = test_transforms(image).float()
+    image_tensor = image_tensor.unsqueeze_(0)
+    output = model(input)
+    index = output.data.numpy().argmax()
+    return index
+
+
+images = []
+for  in os.listdir(folder):
+    img = cv2.imread(os.path.join(folder,filename))
+    if img is not None:
+        images.append(img)
+
+classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+folder = 'test_images/'
+
+for filename in os.listdir(folder):
+    img = cv2.imread(os.path.join(folder,filename))
+    if img is None: continue
+    st = time()
+    pred = classes[predict_image(img)]
+    print(f'{filename} is for "{pred}"')
+    print(f'Inference took {time()-st} s')
